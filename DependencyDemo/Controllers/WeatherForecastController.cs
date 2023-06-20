f30d0b481154c23d6ec932bbd18230d8df81791f
yee <yee@softwareangels.com.au>
2023-06-20 22:45:19 +0700

    Add controllers

--- DependencyDemo/Controllers/WeatherForecastController.cs
+++ DependencyDemo/Controllers/WeatherForecastController.cs
@@ -1,33 +1,79 @@
+using DependencyDemo.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DependencyDemo.Controllers
+namespace DependencyDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SingletonController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private readonly ISingleton _singleton1;
+    private readonly ISingleton _singleton2;
+    public SingletonController(ISingleton singleton1, ISingleton singleton2)
     {
-        private static readonly string[] Summaries = new[]
+        _singleton1 = singleton1;
+        _singleton2 = singleton2;
+    }
+
+    [HttpGet(Name = "GetWeatherForecastSingleton")]
+    public IActionResult Get()
+    {
+        var result = new
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            Singleton1 = _singleton1.GetHashCode(),
+            Singleton2 = _singleton2.GetHashCode()
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        return Ok(result);
+    }
+}
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+[ApiController]
+[Route("[controller]")]
+public class ScopeController : ControllerBase
+{
+    private readonly IScoped _scope1;
+    private readonly IScoped _scope2;
+    public ScopeController(IScoped scope1, IScoped scope2)
+    {
+        _scope1 = scope1;
+        _scope2 = scope2;
+    }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "GetWeatherForecastScope")]
+    public IActionResult Get()
+    {
+        var result = new
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+            Scope1 = _scope1.GetHashCode(),
+            Scope2 = _scope2.GetHashCode()
+        };
+
+        return Ok(result);
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class TransientController : ControllerBase
+{
+    private readonly ITransient _transient1;
+    private readonly ITransient _transient2;
+    public TransientController(ITransient transient1, ITransient transient2)
+    {
+        _transient1 = transient1;
+        _transient2 = transient2;
+    }
+
+    [HttpGet(Name = "GetWeatherForecastTransient")]
+    public IActionResult Get()
+    {
+        var result = new
+        {
+            Transient1 = _transient1.GetHashCode(),
+            Transient2 = _transient2.GetHashCode()
+        };
+
+        return Ok(result);
     }
 }
---
+++ DependencyDemo/Interfaces/ISingleton.cs
@@ -0,0 +1,55 @@
+﻿namespace DependencyDemo.Interfaces;
+
+public interface ISingleton
+{
+
+}
+
+public class Singleton : ISingleton, IDisposable
+{
+    public Singleton()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
+
+public interface IScoped
+{
+
+}
+
+public class Scoped : IScoped, IDisposable
+{
+    public Scoped()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+        
+    }
+}
+
+public interface ITransient
+{
+
+}
+
+public class Transient : ITransient, IDisposable
+{
+    public Transient()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
