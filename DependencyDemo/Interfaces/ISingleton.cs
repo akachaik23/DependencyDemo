036fe14af23ebea150e979332698cd1ea5f27e99
akachai <yee063@hotmail.com>
2023-07-25 21:29:38 +0700

    Add message for constructure and dispose

--- DependencyDemo/Interfaces/ISingleton.cs
+++ DependencyDemo/Interfaces/ISingleton.cs
@@ -9,12 +9,12 @@ public class Singleton : ISingleton, IDisposable
 {
     public Singleton()
     {
-        
+        Console.WriteLine("Singleton Constructure");
     }
 
     public void Dispose()
     {
-
+        Console.WriteLine("Singleton Dispose");
     }
 }
 
@@ -27,12 +27,12 @@ public class Scoped : IScoped, IDisposable
 {
     public Scoped()
     {
-        
+        Console.WriteLine("Scoped Constructure");
     }
 
     public void Dispose()
     {
-        
+        Console.WriteLine("Scoped Dispose");
     }
 }
 
@@ -45,11 +45,11 @@ public class Transient : ITransient, IDisposable
 {
     public Transient()
     {
-        
+        Console.WriteLine("Transient Constructure");
     }
 
     public void Dispose()
     {
-
+        Console.WriteLine("Transient Dispose");
     }
 }
